11d57377aae7155ce0a44d2aa9afd2cb4b6a9a5c
marcelabueeduar <marcela.cerda@bue.edu.ar>
2022-09-26 21:44:24 -0300

    version1

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
 })
-export class AppComponent {
-  /*title = 'Portfolio';
+export class MainComponent implements OnInit {
+  title = 'Portfolio';
   nombre="Marcela"
   apellido="Cerda"
   foto="https://lh3.googleusercontent.com/a-/ACNPEu_r2EBdnJP-CDlquVpuxwxt8DDkrEThdDaXH38b=s288-p-rw-no"
   hijos=[
     {nombre:"Santiago", fechaNacimiento:"01-01-1995", estudios:" bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla"},
     {nombre:"Florencia", fechaNacimiento:"01-01-1995", estudios:" bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla"},
-  ]*/
+  ] 
+ 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
